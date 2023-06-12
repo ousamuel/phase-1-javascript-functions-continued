cd4a80284aba99b2e40a7e68a8bc7c8e79be10ad
Samuel <samuelou510@gmail.com>
2023-06-12 18:43:02 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(act = 'roller-skate'){
+  return `This Saturday, I want to ${act}!`;
+}
+
+function mondayWork(act = 'go to the office'){
+  return `This Monday, I will ${act}.`;
+}
+
+function wrapAdjective(str = '*'){
+  return function result(adj = 'special'){
+    const first = `You are ${str}${adj}${str}!`;
+    return first;
+  }
+  
+}
+console.log(wrapAdjective('??')("the best"));
